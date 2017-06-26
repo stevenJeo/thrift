b383d9b5e32c0e6a0c0e056c3c91e0b8da128104
zhouzhishuai <zhishuai.zhou@wormpex.com>
2017-06-26 10:32:13 +0800

    update from mac

--- src/main/java/com/blue/thrift/algorithm/Knapsack.java
+++ src/main/java/com/blue/thrift/algorithm/Knapsack.java
@@ -7,4 +7,7 @@ public class Knapsack {
 
     //http://www.cnblogs.com/lfeng1205/p/5981198.html
 //    http://blog.csdn.net/u014394715/article/details/51162960
+    // update from mac
+    //来自mac的更新。。。
+    //来自mac的更新。。。
 }
