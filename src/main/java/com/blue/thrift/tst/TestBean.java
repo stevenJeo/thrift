954461077723e5c1ec70e52aa2a468fbeccc0c0a
stevenJeo <864813905@qq.com>
2017-07-11 21:07:23 +0800

    GitHub update

--- src/main/java/com/blue/thrift/tst/TestBean.java
+++ src/main/java/com/blue/thrift/tst/TestBean.java
@@ -12,7 +12,7 @@ public class TestBean {
         double a1 = 0.1;
         double da = 3;
         double aa = 3*0.1;
-        System.out.print(aa+"\n");
+        System.out.print(da+aa+"da,..\n");
 
         System.out.print(3*0.1);
         System.out.print(3*0.1==0.3);
