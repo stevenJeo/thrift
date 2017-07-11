f3353434802d10afc3993c9e55b4c6df778fcffd
zhishuai.zhou <zhishuai.zhou@wormpex.com>
2017-07-11 21:11:01 +0800

    local

---
+++ src/main/java/com/blue/thrift/thread/pool/ScheduleThreaPoolExecutorTest.java
@@ -0,0 +1,7 @@
+package com.blue.thrift.thread.pool;
+
+/**
+ * Created by zhouzhishuai on 2017/7/5.
+ */
+public class ScheduleThreaPoolExecutorTest {
+}
---
+++ src/main/java/com/blue/thrift/tst/TTTT.java
@@ -0,0 +1,27 @@
+package com.blue.thrift.tst;
+
+//import org.springframework.util.StringUtils;
+
+//import org.apache.commons.codec.binary.StringUtils;
+
+import org.apache.tomcat.util.codec.binary.StringUtils;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by zhouzhishuai on 2017/7/7.
+ */
+public class TTTT {
+
+    public static void  main(String[] args){
+
+        BigDecimal bigDecimal = new BigDecimal("-32.17");
+
+        System.out.print("bigDecimal="+bigDecimal);
+//        StringUtils.
+        //sad
+
+    }
+
+
+}
